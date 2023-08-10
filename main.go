341243961a6c15cd1d182d3069c919f85b7148f4
felipereyel <felipe.reyel@gmail.com>
2023-08-10 18:16:07 -0300

    migrations!

--- main.go
+++ main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
-	"fintracker/pkgs/handlers"
 	"log"
 	"os"
+	"vuepb/pkgs/config"
+	"vuepb/pkgs/handlers"
+	_ "vuepb/pkgs/migrations"
 
-	_ "github.com/joho/godotenv/autoload"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
 func main() {
 	app := pocketbase.New()
-	publicDir := os.Getenv("PUBLIC_DIR")
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		Automigrate: config.Envs.Automigrate,
+	})
 
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.GET("/healthz", handlers.Healthz)
-		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS(publicDir), true))
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS(config.Envs.PublicDir), true))
 		return nil
 	})
 
---
+++ pkgs/migrations/1691696395_created_notes.go
@@ -0,0 +1,74 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		jsonData := `{
+			"id": "lr9w1r5itroply9",
+			"created": "2023-08-10 19:39:55.026Z",
+			"updated": "2023-08-10 19:39:55.026Z",
+			"name": "notes",
+			"type": "base",
+			"system": false,
+			"schema": [
+				{
+					"system": false,
+					"id": "03zkbhln",
+					"name": "title",
+					"type": "text",
+					"required": false,
+					"unique": false,
+					"options": {
+						"min": null,
+						"max": null,
+						"pattern": ""
+					}
+				},
+				{
+					"system": false,
+					"id": "ohpmjuex",
+					"name": "content",
+					"type": "text",
+					"required": false,
+					"unique": false,
+					"options": {
+						"min": null,
+						"max": null,
+						"pattern": ""
+					}
+				}
+			],
+			"indexes": [],
+			"listRule": "",
+			"viewRule": "",
+			"createRule": "",
+			"updateRule": "",
+			"deleteRule": "",
+			"options": {}
+		}`
+
+		collection := &models.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return daos.New(db).SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db)
+
+		collection, err := dao.FindCollectionByNameOrId("lr9w1r5itroply9")
+		if err != nil {
+			return err
+		}
+
+		return dao.DeleteCollection(collection)
+	})
+}
